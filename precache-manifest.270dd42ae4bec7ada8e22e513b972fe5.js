559e9ed09b0b4401290cd333c4f280a1f4213e9a
Mulualem Eshetu <mulualem@ienetworks.co>
2022-08-31 15:22:14 +0300

    Updates

---
+++ precache-manifest.270dd42ae4bec7ada8e22e513b972fe5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b2305022c96c214217e",
+    "url": "/static/js/main.8b230502.chunk.js"
+  },
+  {
+    "revision": "7799d9e812df4e12802c",
+    "url": "/static/js/1.7799d9e8.chunk.js"
+  },
+  {
+    "revision": "47263aaaeb2311d54a24",
+    "url": "/static/js/2.47263aaa.chunk.js"
+  },
+  {
+    "revision": "8cf682de8d1741a7ddd6",
+    "url": "/static/js/runtime~main.8cf682de.js"
+  },
+  {
+    "revision": "500fccecf260c6391d7f66b4496b1017",
+    "url": "/static/media/mule.500fccec.jpg"
+  },
+  {
+    "revision": "e5a53ac68a4be2a81a38ebe310d7c58a",
+    "url": "/static/media/sura.e5a53ac6.avif"
+  },
+  {
+    "revision": "d4219aa9b94474175688475ec4616183",
+    "url": "/static/media/abrish.d4219aa9.avif"
+  },
+  {
+    "revision": "5a629fff5583f9ab5f0931d14736b299",
+    "url": "/static/media/postImage.5a629fff.png"
+  },
+  {
+    "revision": "a720bb0f198db9c781a6852c63da5f25",
+    "url": "/static/media/zemen.a720bb0f.avif"
+  },
+  {
+    "revision": "cfdc7f60c7faabde39e94eb62801eefd",
+    "url": "/static/media/nardi.cfdc7f60.avif"
+  },
+  {
+    "revision": "6f632ca3ce562ba7901350380d555845",
+    "url": "/static/media/kira.6f632ca3.avif"
+  },
+  {
+    "revision": "bacf5750d13f9f474a898f544fe878d5",
+    "url": "/static/media/abdu.bacf5750.avif"
+  },
+  {
+    "revision": "d8e27b004f34efa47bfd24209ce79b4d",
+    "url": "/static/media/img1.d8e27b00.jpg"
+  },
+  {
+    "revision": "aa3b999c0b10886148b84ffe5fec063d",
+    "url": "/static/media/img2.aa3b999c.jpg"
+  },
+  {
+    "revision": "34f74d9468d238af2e1196188980738d",
+    "url": "/static/media/img3.34f74d94.jpg"
+  },
+  {
+    "revision": "495b15436dd55432651f0bef6e5926a5",
+    "url": "/static/media/img4.495b1543.jpg"
+  },
+  {
+    "revision": "9a660371ef1b621192ba4cf716c991c7",
+    "url": "/static/media/img5.9a660371.jpg"
+  },
+  {
+    "revision": "e0993c641e39bbbfb473c596e804cb6c",
+    "url": "/index.html"
+  }
+];
